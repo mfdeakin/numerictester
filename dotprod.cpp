7e107b41647480d76fec0d7363c0cbf370003e22
mfdeakin <mdever44@gmail.com>
2015-09-01 09:58:39 -0400

    Basic test case working

--- dotprod.cpp
+++ dotprod.cpp
@@ -3,6 +3,10 @@
 #include "mpreal.h"
 
 #include <random>
+#include <typeinfo>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
 #include <assert.h>
 #include <time.h>
@@ -10,14 +14,16 @@
 template <typename fptype>
 class DotProdTest;
 
-template <typename fptype, unsigned dim, unsigned precision>
+template <typename fptype>
 class DotProdCase : public NumericTester::TestCase {
  public:
   DotProdCase(std::mt19937_64 &rgen,
-              std::uniform_real_distribution<fptype> &dist)
+              std::uniform_real_distribution<fptype> &dist,
+              unsigned dim, unsigned precision)
       : NumericTester::TestCase(precision),
         v1(new fptype[dim]),
-        v2(new fptype[dim]) {
+        v2(new fptype[dim]),
+        dim(dim) {
     mpfr::mpreal val1(precision), val2(precision);
     for(unsigned i = 0; i < dim; i++) {
       v1[i] = dist(rgen);
@@ -34,26 +40,41 @@ class DotProdCase : public NumericTester::TestCase {
   }
 
   template <typename>
-  friend class DotProdTest;
+  friend class DPNaiveTest;
 
  private:
   fptype *v1;
   fptype *v2;
-
-  constexpr static const int _dim = dim;
+  const unsigned dim;
 };
 
 template <typename fptype>
-class DotProdTest : public NumericTester::NumericTest {
+class DPNaiveTest : public NumericTester::NumericTest {
  public:
-  virtual void updateStats(const DotProdCase &dpCase) {
-		startTimer();
-		fptype accumulator = 0.0;
-		for(int i = 0; i < dpCase._dim; i++)
-			accumulator += dpCase.v1[i] * dpCase.v2[i];
-		stopTimer();
-		mpfr::mpreal err = dpCase.correctValue() - accumulator;
-	}
+  virtual void updateStats(
+      const NumericTester::TestCase &testCase) {
+    assert(typeid(testCase) ==
+           typeid(const DotProdCase<fptype>));
+    const DotProdCase<fptype> *dpCase =
+        static_cast<const DotProdCase<fptype> *>(&testCase);
+    startTimer();
+    fptype accumulator = 0.0;
+    for(int i = 0; i < dpCase->dim; i++)
+      accumulator += dpCase->v1[i] * dpCase->v2[i];
+    stopTimer();
+    mpfr::mpreal absErr =
+        abs(dpCase->correctValue() - accumulator);
+    absErrors.push_back(absErr);
+    mpfr::mpreal relErr =
+        abs(absErr / dpCase->correctValue());
+    relErrors.push_back(relErr);
+    std::cout << "Absolute Error: " << absErr << "\n"
+              << "Relative Error: " << relErr << "\n"
+              << "Correct Bits: " << log(relErr) << "\n"
+              << "Run Time: " << runningTime.tv_sec << "."
+              << std::setfill('0') << std::setw(9)
+              << runningTime.tv_nsec << " s\n";
+  }
 };
 
 int main(int argc, char **argv) {
@@ -63,6 +84,8 @@ int main(int argc, char **argv) {
   std::mt19937_64 engine(rd());
   std::uniform_real_distribution<float> rgenf(-maxMag,
                                               maxMag);
-  DotProdCase<float, 4, 256> t1(engine, rgenf);
+  DotProdCase<float> t1(engine, rgenf, 4, 512);
+  DPNaiveTest<float> naive;
+  naive.updateStats(t1);
   return 0;
 }
