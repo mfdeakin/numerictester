e990f98a4390b73a59eed7a54ba257254605455f
mfdeakin <mdever44@gmail.com>
2015-09-05 21:31:54 -0400

    Added FMA Kahan dot product test, removed compiler optimizations to try and unbias the results, added Null test to verify timings are wrong

--- dotprod.cpp
+++ dotprod.cpp
@@ -39,11 +39,33 @@ class DotProdCase : public NumericTester::TestCase {
   friend class DPNaiveTest;
   template <typename>
   friend class DPFMATest;
+  template <typename>
+  friend class DPFMAKahanTest;
 
  private:
   fptype *v1;
   fptype *v2;
   const unsigned dim;
+};
+
+template <typename fptype>
+class DPNullTest : public NumericTester::NumericTest {
+ public:
+  virtual const char *testName() {
+    return "Null Dot Product";
+  }
+
+  virtual void updateStats(
+      const NumericTester::TestCase &testCase) {
+    assert(typeid(testCase) ==
+           typeid(const DotProdCase<fptype>));
+    const DotProdCase<fptype> *dpCase =
+        static_cast<const DotProdCase<fptype> *>(&testCase);
+    startTimer();
+    stopTimer();
+    mpfr::mpreal estimate(0);
+    addStatistic(estimate, testCase.correctValue());
+  }
 };
 
 template <typename fptype>
@@ -93,6 +115,34 @@ class DPFMATest : public NumericTester::NumericTest {
   }
 };
 
+template <typename fptype>
+class DPFMAKahanTest : public NumericTester::NumericTest {
+ public:
+  virtual const char *testName() {
+    return "Kahan FMA Dot Product";
+  }
+
+  virtual void updateStats(
+      const NumericTester::TestCase &testCase) {
+    assert(typeid(testCase) ==
+           typeid(const DotProdCase<fptype>));
+    const DotProdCase<fptype> *dpCase =
+        static_cast<const DotProdCase<fptype> *>(&testCase);
+    startTimer();
+    fptype accumulator = 0.0;
+    fptype c = 0.0;
+    for(unsigned i = 0; i < dpCase->dim; i++) {
+      fptype mod = std::fma(dpCase->v1[i], dpCase->v2[i], -c);
+      fptype tmp = accumulator + mod;
+      c = (tmp - accumulator) - mod;
+      accumulator = tmp;
+    }
+    stopTimer();
+    mpfr::mpreal estimate(accumulator);
+    addStatistic(estimate, testCase.correctValue());
+  }
+};
+
 int main(int argc, char **argv) {
   mpfr::mpreal::set_default_prec(1024);
   typedef double fptype;
@@ -101,15 +151,21 @@ int main(int argc, char **argv) {
   std::mt19937_64 engine(rd());
   std::uniform_real_distribution<float> rgenf(-maxMag,
                                               maxMag);
+  DPNullTest<float> null;
   DPNaiveTest<float> naive;
   DPFMATest<float> fmaTester;
-  for(int i = 0; i < 1e6; i++) {
+  DPFMAKahanTest<float> fmaKahanTester;
+  for(int i = 0; i < 1e5; i++) {
     DotProdCase<float> test(engine, rgenf, 4);
+    null.updateStats(test);
     naive.updateStats(test);
     fmaTester.updateStats(test);
+    fmaKahanTester.updateStats(test);
   }
+  null.printStats();
   naive.printStats();
   fmaTester.printStats();
+  fmaKahanTester.printStats();
   {
     std::ofstream naiveResults("Naive_Results.csv",
                                std::ios::out);
@@ -119,6 +175,11 @@ int main(int argc, char **argv) {
     std::ofstream fmaResults("FMA_Results.csv",
                              std::ios::out);
     fmaTester.dumpData(fmaResults);
+  }
+  {
+    std::ofstream fmaKahanResults("FMA_Kahan_Results.csv",
+                             std::ios::out);
+    fmaKahanTester.dumpData(fmaKahanResults);
   }
   return 0;
 }
